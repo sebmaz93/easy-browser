22c6e2bcef9c27896af79f59131f1f4f3294bd6f
sebmaz <titan9389@me.com>
2021-03-23 19:15:58 +0400

    add gitignore

---
+++ easy browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  easy browser
+//
+//  Created by Sebouh Mazloumian on 23.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
